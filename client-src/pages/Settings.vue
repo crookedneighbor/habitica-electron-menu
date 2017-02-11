236da726bb3a321dbaccc2a61b92827a4317c5cd
Blade Barringer <blade@crookedneighbor.com>
2017-02-10 22:10:30 -0600

    Add link to exit from settings

--- client-src/pages/Settings.vue
+++ client-src/pages/Settings.vue
@@ -1,6 +1,9 @@
 <template>
 <div class="ui segment">
   <div class="ui form">
+    <router-link to="/">
+      <div class="ui button primary">Exit</div>
+    </router-link>
     <router-link to="/logout">
       <div class="ui button">Logout</div>
     </router-link>
