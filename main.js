5d4633de33871e18cbb01ee814387bc4d838b554
Blade Barringer <blade@crookedneighbor.com>
2017-02-11 22:56:21 -0600

    Enable copy/paste etc

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
+'use strict'
+
 const menubar = require('menubar')
 const mb = menubar()
 
+const BrowserWindow = require('browser-window');
+const Menu = require('menu');
+
+let mainWindow = null;
 
 mb.on('ready', function ready () {
   console.log('app is ready')
+
+  const template = [{
+    label: "Application",
+    submenu: [
+      { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+    ]}, {
+      label: "Edit",
+      submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+      ]}
+  ];
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 })
 
 if (process.env.NODE_ENV === 'development') {
